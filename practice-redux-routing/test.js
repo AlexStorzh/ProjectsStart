36a4d47147372b55324b45ef784af2532e31dad9
AlexStorzh <ktgcov57@gmail.com>
2022-11-22 20:31:41 +0200

    Final stage, need to add Inventory

--- practice-redux-routing/test.js
+++ practice-redux-routing/test.js
@@ -1,4 +1,4 @@
-<div className={style.finalCharacter_title}>Your Character Modifires</div>
+
         <div className={style.finalCharacter_modifires}>
        {finalCharacterAttributes.map((e) => {
          return (
