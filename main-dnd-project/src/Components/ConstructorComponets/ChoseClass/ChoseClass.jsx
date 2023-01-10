9e716476fbb5d5bbaa84cbfd1693dac6c4044357
AlexStorzh <ktgcov57@gmail.com>
2023-01-10 17:49:47 +0200

    Check

--- main-dnd-project/src/Components/ConstructorComponets/ChoseClass/ChoseClass.jsx
+++ main-dnd-project/src/Components/ConstructorComponets/ChoseClass/ChoseClass.jsx
@@ -1,54 +1,52 @@
-import React from 'react';
-import { useEffect, useState } from 'react'
-import style from './ChoseClass.module.css'
-import { getApiResource } from '../../../api/api';
-import { DND_ROOT_CLASSES, DND } from '../../../constants/constantsApi';
-
+import React from "react";
+import { useEffect, useState } from "react";
+import style from "./ChoseClass.module.css";
+import { getApiResource } from "../../../api/api";
+import { DND_ROOT_CLASSES, DND } from "../../../constants/constantsApi";
 
 const Class = ({ page, setPage, formData, setFormData }) => {
- const [classes, setClasses] = useState()
-  console.log(formData);
+  const [classes, setClasses] = useState();
+  // console.log(formData);
 
- const getResource = async (url) => {
-  const res = await getApiResource(url)
-  setClasses(res.results)
- }
+  const getResource = async (url) => {
+    const res = await getApiResource(url);
+    setClasses(res.results);
+  };
 
- useEffect(() => { 
-  getResource(DND_ROOT_CLASSES)
- }, []);
+  useEffect(() => {
+    getResource(DND_ROOT_CLASSES);
+  }, []);
 
- 
- const handleClick = async (e, url) => {
-  const res = await getApiResource(DND + url)
-  setFormData({ ...formData, class: res })
-  setPage(page + 1)
- }
- return (
-  <>
-   {classes && (
-   <div className={style.template}>
-     {classes.map(({ index, name, url }) =>
-      <div
-     className={style.card}
-       key={index}
-       onClick={(e) => handleClick(e, url)}
-    >
-     <h1> {name}</h1> 
-    </div> 
-    )}
-    
-   </div>
-  )}
-    <button className={style.button} onClick={() => {
-     setPage(page - 1)
-    }}>
+  const handleClick = async (e, url) => {
+    const res = await getApiResource(DND + url);
+    setFormData({ ...formData, class: res });
+    setPage(page + 1);
+  };
+  return (
+    <>
+      {classes && (
+        <div className={style.template}>
+          {classes.map(({ index, name, url }) => (
+            <div
+              className={style.card}
+              key={index}
+              onClick={(e) => handleClick(e, url)}
+            >
+              <h1> {name}</h1>
+            </div>
+          ))}
+        </div>
+      )}
+      <button
+        className={style.button}
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
         Previous
       </button>
-  </>
-
- );
+    </>
+  );
 };
 
 export default Class;
-
--- main-dnd-project/src/Components/ConstructorComponets/ChoseRace/ChoseRace.jsx
+++ main-dnd-project/src/Components/ConstructorComponets/ChoseRace/ChoseRace.jsx
@@ -1,59 +1,62 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { getApiResource } from '../../../api/api';
-import style from './ChoseRace.module.css'
-import { DND_ROOT_RACES, DND } from '../../../constants/constantsApi';
-import ModalWindow from './ModalWindow.jsx'
+import React from "react";
+import { useEffect, useState } from "react";
+import { getApiResource } from "../../../api/api";
+import style from "./ChoseRace.module.css";
+import { DND_ROOT_RACES, DND } from "../../../constants/constantsApi";
+import ModalWindow from "./ModalWindow.jsx";
 
 const ChoseRace = ({ page, setPage, formData, setFormData }) => {
- const [races, setRaces] = useState();
- const [modalVisibility, setModalVisibility] = useState(false)
+  const [races, setRaces] = useState();
+  const [modalVisibility, setModalVisibility] = useState(false);
 
- 
- const getResource = async (url) => {
-  const res = await getApiResource(url)
-  setRaces(res.results)
- }
- useEffect(() => { 
-  getResource(DND_ROOT_RACES)
- }, []);
- 
+  const getResource = async (url) => {
+    const res = await getApiResource(url);
+    setRaces(res.results);
+  };
+  useEffect(() => {
+    getResource(DND_ROOT_RACES);
+  }, []);
+
   const handeClick = async (e, url) => {
-   const res = await getApiResource(DND + url)
-   setFormData({ ...formData, race: res })
-   setModalVisibility(true)
-  }
+    console.log(e, url);
+    const res = await getApiResource(DND + url);
+    setFormData({ ...formData, race: res });
+    setModalVisibility(true);
+  };
 
-
- return (
-  <>
-     
-   {races && (
-    <div className={style.template}>
-    {races.map(({index, name, url}) =>
-     <div
-     className={style.card}
-      key={index}
-      onClick={(e) => handeClick(e, url)}
-    >
-      <h1 className={style.h1 }> {name}</h1>
-     </div> 
-     
-   )}
-    </div>)}
-   {modalVisibility &&
-    <ModalWindow
-    page = {page}
-    setPage ={setPage}
-    race={formData.race}
-    setActive={setModalVisibility}/>}
-   <button className={style.button} onClick={() => {
-     setPage(page - 1)
-    }}>
+  return (
+    <>
+      {races && (
+        <div className={style.template}>
+          {races.map(({ index, name, url }) => (
+            <div
+              className={style.card}
+              key={index}
+              onClick={(e) => handeClick(e, url)}
+            >
+              <h1 className={style.h1}> {name}</h1>
+            </div>
+          ))}
+        </div>
+      )}
+      {modalVisibility && (
+        <ModalWindow
+          page={page}
+          setPage={setPage}
+          race={formData.race}
+          setActive={setModalVisibility}
+        />
+      )}
+      <button
+        className={style.button}
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
         Previous
       </button>
-   </>
- );
+    </>
+  );
 };
 
 export default ChoseRace;
